d0152e6ff4643349506663afc98951fa67d58983
travisbrigman <travis1000@gmail.com>
2020-11-04 16:01:56 -0600

    working on seeding database

--- raterprojectapi/models/Game.py
+++ raterprojectapi/models/Game.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class Game(models.Model):
     """Game database model"""
     user = models.ForeignKey(User,  on_delete=models.CASCADE)
-    release_year = models.IntegerField
+    release_year = models.IntegerField()
     game_title = models.CharField(max_length=75)
