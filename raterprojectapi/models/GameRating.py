c04f35a75d7693b69b881d1e4a3edab47591d0d4
travisbrigman <travis1000@gmail.com>
2020-11-05 15:58:34 -0600

    got data properly seeded

--- raterprojectapi/models/GameRating.py
+++ raterprojectapi/models/GameRating.py
@@ -5,5 +5,5 @@ class GameRating(models.Model):
     """GameRating database model"""
     user = models.ForeignKey(User,  on_delete=models.CASCADE)
     game = models.ForeignKey("Game",  on_delete=models.CASCADE)
-    user_rating = models.IntegerField
+    user_rating = models.IntegerField()
     game_review = models.CharField(max_length=500)
